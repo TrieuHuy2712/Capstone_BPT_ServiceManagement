ae1e9120a3f9708d2110283646521221c4d4f1ea
huytrieu <huytrieu@kms-techonlogy.com>
2020-04-17 23:01:41 +0700

    add something

--- Front-end/BPT-Service-FrontEnd/src/app/core/common/system,constants.ts
+++ Front-end/BPT-Service-FrontEnd/src/app/core/common/system,constants.ts
@@ -1,7 +1,7 @@
 export class SystemConstants{
     public static CURRENT_USER="CURRENT_USER";
     //public static BASE_API="http://192.168.1.7";
-    public static BASE_API="http://localhost:5000";
+    public static BASE_API="https://bpt-servicewebapi20200417011418.azurewebsites.net";
     public static const_permission="";
     public static const_username="const_username";
 }
