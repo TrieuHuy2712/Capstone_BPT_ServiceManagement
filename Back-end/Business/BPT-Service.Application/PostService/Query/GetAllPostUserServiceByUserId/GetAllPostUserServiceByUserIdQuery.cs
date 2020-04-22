8b294b1a68ed9da63a5bb1295b4fba2f85934947
Phuoc Toan <t153067@vanlanguni.vn>
2020-04-22 22:40:00 +0700

    create API get all Post from userid

---
+++ Back-end/Business/BPT-Service.Application/PostService/Query/GetAllPostUserServiceByUserId/GetAllPostUserServiceByUserIdQuery.cs
@@ -0,0 +1,39 @@
+﻿using BPT_Service.Application.PostService.ViewModel;
+using BPT_Service.Model.Entities.ServiceModel;
+using BPT_Service.Model.Infrastructure.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BPT_Service.Application.PostService.Query.GetAllPostUserServiceByUserId
+{
+    public class GetAllPostUserServiceByUserIdQuery : IGetAllPostUserServiceByUserIdQuery
+    {
+        private readonly IRepository<Model.Entities.ServiceModel.UserServiceModel.UserService, int> _userServiceRepository;
+        private readonly IRepository<Service, Guid> _serviceRepository;
+        public GetAllPostUserServiceByUserIdQuery(
+            IRepository<Model.Entities.ServiceModel.UserServiceModel.UserService, int> userServiceRepository,
+            IRepository<Service, Guid> serviceRepository)
+        {
+            _userServiceRepository = userServiceRepository;
+            _serviceRepository = serviceRepository;
+        }
+
+        public async Task<List<ListServiceViewModel>> ExecuteAsync(string idUser)
+        {
+            var findByUserId = await _userServiceRepository.FindAllAsync(x => x.UserId == Guid.Parse(idUser));
+            if (findByUserId == null)
+            {
+                return null;
+            }
+            var data = findByUserId.Select(x => new ListServiceViewModel
+            {
+                Id = x.ServiceId
+            }).ToList();
+
+            
+            return data;
+        }
+    }
+}
